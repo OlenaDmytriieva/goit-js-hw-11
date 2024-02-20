9c62ff616eb17bc01ff766dfb93a0b18c4d8c2fc
OlenaDmytriieva <lela.dmitrieva@gmail.com>
2024-02-20 21:32:32 +0200

    fixed

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,12 +1,12 @@
-'use strict';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+import { createGallery } from './render-functions';
 
 const form = document.querySelector('.search-form');
 const loaderElement = document.querySelector('.loader');
-let myGallery;
+export let myGallery;
 
 form.addEventListener('submit', e => {
   e.preventDefault();
@@ -75,45 +75,3 @@ form.addEventListener('submit', e => {
     clearInterval(loadingInterval);
   }
 });
-
-function createGallery(images) {
-  const result = images
-    .map(image => {
-      return `<li class="gallery-item">
-                        <div class="box">
-                          <a class="gallery-link" href="${image.largeImageURL}">
-                            <img class="gallery-image" src="${image.webformatURL}" alt="${image.tags}" data-likes="${image.likes}" data-views="${image.views}" data-comments="${image.comments}" data-downloads="${image.downloads}"/>
-                            <div class="img-card">
-                              <div class="img-data">
-        <h4 class="data-title">Likes</h4>
-        <p class="data-value">
-          ${image.likes}
-        </p>
-      </div>
-      <div class="img-data">
-        <h4 class="data-title">Views</h4>
-        <p class="data-value">
-          ${image.views}
-        </p>
-      </div>
-      <div class="img-data">
-        <h4 class="data-title">Comments</h4>
-        <p class="data-value">
-          ${image.comments}
-        </p>
-      </div>
-      <div class="img-data">
-        <h4 class="data-title">Downloads</h4>
-        <p class="data-value">
-          ${image.downloads}
-        </p>
-      </div>
-                            </div>
-                          </a>
-                        </div>
-                      </li>`;
-    })
-    .join('\n');
-
-  myGallery.innerHTML = result;
-}
